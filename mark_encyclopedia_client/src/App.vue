e51d17a1433b48a9991f9c1f53593ef54b593313
LLIypKa <shilkoaa@gmail.com>
2024-09-29 14:53:02 +0700

    added bg into page

--- mark_encyclopedia_client/src/App.vue
+++ mark_encyclopedia_client/src/App.vue
@@ -1,18 +1,30 @@
 <template>
   <v-app>
-    <v-main>
+    <v-main :style="{ backgroundImage: `url(${BackgroundImage})` }">
       <router-view/>
     </v-main>
   </v-app>
 </template>
 
 <script>
+import BackgroundImage from "@/assets/mark2110_bgimage.jpg";
 
 export default {
   name: 'App',
 
-  data: () => ({
-    //
-  }),
+  data() {
+    return {
+      BackgroundImage
+    };
+  },
 }
 </script>
+
+<style scoped>
+.v-main {
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  height: 100vh;
+}
+</style>
