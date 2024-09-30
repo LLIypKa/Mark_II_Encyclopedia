df14aa4070498f0e4c03446a68c6976176a9f6c8
LLIypKa <shilkoaa@gmail.com>
2024-10-01 00:55:40 +0700

    fixed login form style v1

--- mark_encyclopedia_client/src/components/LoginComponent.vue
+++ mark_encyclopedia_client/src/components/LoginComponent.vue
@@ -1,11 +1,11 @@
 <template>
-    <v-container>
+    <v-container class="transparentForm">
         <v-card>
-            <v-card-title style="align-content: center;">Авторизация</v-card-title>
+            <v-card-title class="v-card-title">Авторизация</v-card-title>
             <v-card-text>
                 <v-form ref = "form" v-model="valid">
-                    <v-text-field label = "Email" v-model="email" :rules="emailRules" type="email" required/>
-                    <v-text-field label = "Password" v-model="password" :rules="passwordRules" type="password" required/>
+                    <v-text-field label = "Email" v-model="email" :rules="emailRules" type="email" required class="v-text-field"/>
+                    <v-text-field label = "Password" v-model="password" :rules="passwordRules" type="password" required class="v-text-field"/>
                     <v-btn type = "submit" text = "Войти" color="primary" @click="login" :disabled="!valid"/>
                 </v-form>
             </v-card-text>
@@ -39,3 +39,41 @@
         }
     }
 </script>
+
+<style lang="css" scoped>
+    .transparentForm {
+        box-sizing: border-box;
+        background: rgba( 0,0,0,.6);
+        border-radius: 5px;
+        width: 400px;
+        padding: 40px;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        font-family: 'Gabriola';
+        justify-content: center;
+    }
+    .v-card-title {
+        color: #fff;
+        font-size: 50px;     
+        padding: 0;
+        text-align: center;
+        margin-bottom: 20px; 
+    }
+    .v-text-field {
+        background: transparent;
+        border: none;
+        border-bottom: 1px solid #fff;
+        color: #fff;
+        font-size: 20px;
+    }
+    .v-btn {
+        font-size: 20px; 
+        width: 100%;
+    }
+    .v-card {
+        background: transparent; /* Прозрачный фон для карточки */
+        box-shadow: none; /* Убираем тень, чтобы не было визуального блока */
+    }
+</style>
