7f7d2ef95c49658e01cf3497808e39c1230aee3a
LLIypKa <shilkoaa@gmail.com>
2024-10-01 00:39:46 +0700

    added login page and correct router

---
+++ mark_encyclopedia_client/src/components/LoginComponent.vue
@@ -0,0 +1,41 @@
+<template>
+    <v-container>
+        <v-card>
+            <v-card-title style="align-content: center;">Авторизация</v-card-title>
+            <v-card-text>
+                <v-form ref = "form" v-model="valid">
+                    <v-text-field label = "Email" v-model="email" :rules="emailRules" type="email" required/>
+                    <v-text-field label = "Password" v-model="password" :rules="passwordRules" type="password" required/>
+                    <v-btn type = "submit" text = "Войти" color="primary" @click="login" :disabled="!valid"/>
+                </v-form>
+            </v-card-text>
+        </v-card>
+    </v-container>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                email: "",
+                password: "",
+                valid: false,
+                emailRules: [
+                    v => !!v || "Почта обязательна",
+                    v => /.+@.+\..+/.test(v) || "Почта должна быть корректной",
+                ],
+                passwordRules: [
+                    v => !!v || "Пароль обязателен",
+                    v => (v.length >= 6) || "Пароль должен быть длинее 6 символов",
+                ],
+            };
+        },
+        methods: {
+            login() {
+                if (this.$refs.form.validate()) {
+                    alert(this.email)
+                }
+            }
+        }
+    }
+</script>
--- mark_encyclopedia_client/src/router/index.js
+++ mark_encyclopedia_client/src/router/index.js
@@ -1,11 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import LoginComponent from '@/components/LoginComponent.vue'
 
 const routes = [
   {
     path: '/',
+    redirect: '/login'
+  },
+  {
+    path: '/home',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: LoginComponent
   },
   {
     path: '/about',
