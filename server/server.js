f7e26b0d82086e8d45a7ad88af60ec32b48b59c7
LLIypKa <shilkoaa@gmail.com>
2024-10-03 11:21:17 +0700

    add db connection file

--- server/server.js
+++ server/server.js
@@ -7,42 +7,10 @@ const path = require('path')
 const app = express();
 const PORT = 3001;
 
-const dbPath = path.resolve(__dirname, "database/base.db")
-const db = new sqlite.Database(dbPath, (err) => {
-    if (err) {
-        console.error('Ошибка при подключении к базе данных', err.message);
-    } else {
-        console.log('Подключено к базе данных SQLite');
-    }
-});
+const db = require('./database/databaseConnection.js');
 
 app.use(cors());
 app.use(bodyParser.json());
-
-db.serialize(() => {
-    db.run(`
-    CREATE TABLE IF NOT EXISTS users (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      email TEXT NOT NULL UNIQUE,
-      password TEXT NOT NULL,
-      name TEXT NOT NULL
-    )
-  `, (err) => {
-        if (err) {
-            console.error('Ошибка при создании таблицы:', err.message);
-        }
-    });
-});
-
-db.serialize(() => {
-    db.get("SELECT COUNT(*) AS count FROM users", (err, row) => {
-        if (row.count === 0) {
-            db.run(`INSERT INTO users (email, password, name) VALUES ('admin@yandex.ru', 'admin34', 'ApaXuc')`);
-            db.run(`INSERT INTO users (email, password, name) VALUES ('callika@yandex.ru', 'admin35', 'LLIypKa')`);
-            console.log('Тестовые пользователи добавлены');
-        }
-    });
-});
 
 app.post('/register', (req, res) => {
     const { email, password } = req.body;
