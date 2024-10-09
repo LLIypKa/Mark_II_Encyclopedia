a454d1876c6eff00ed411d7263860d974170a2cb
LLIypKa <shilkoaa@gmail.com>
2024-10-09 13:49:38 +0700

    deleted non useful links

---
+++ mark_encyclopedia_client/src/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-container>
+    <label>hihihaha</label>
+  </v-container>
+</template>
+
+<script>
+
+export default {
+  name: 'HelloWorld',
+
+  data() {
+    
+  }
+}
+</script>
